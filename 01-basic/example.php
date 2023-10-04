476640009879aacb811df43d465dcb9df5bf2dae
David <david@grudl.com>
2023-10-04 22:25:09 +0200

    added comments to the code

--- 01-basic/example.php
+++ 01-basic/example.php
@@ -7,9 +7,14 @@ if (@!include __DIR__ . '/../vendor/autoload.php') {
 	exit(1);
 }
 
-
+// Initialize the Latte engine
 $latte = new Latte\Engine;
+
+// Set the temporary directory for compiled templates
 $latte->setTempDirectory(__DIR__ . '/temp');
+
+// Enable auto-refresh for development mode. It recompiles templates on every request.
 $latte->setautoRefresh();
 
+// Render the index.latte template
 $latte->render(__DIR__ . '/templates/index.latte');
--- 02-import/example.php
+++ 02-import/example.php
@@ -7,12 +7,16 @@ if (@!include __DIR__ . '/../vendor/autoload.php') {
 	exit(1);
 }
 
-
+// Initialize the Latte engine
 $latte = new Latte\Engine;
+
+// Set the temporary directory for compiled templates
 $latte->setTempDirectory(__DIR__ . '/temp');
+
+// Enable auto-refresh for development mode
 $latte->setautoRefresh();
 
-
+// Define parameters to be passed to the template
 $params = [
 	'menuItems' => [
 		'About us',
@@ -21,4 +25,5 @@ $params = [
 	],
 ];
 
+// Render the index.latte template with the defined parameters
 $latte->render(__DIR__ . '/templates/index.latte', $params);
